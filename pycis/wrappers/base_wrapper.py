09e25240d468721f3097022f592279e4a69ea088
marcwebbie <marcwebbie@gmail.com>
2013-10-28 23:41:29 -0300

    Changed BaseWrapper name property to strip 'wrapper' from name

--- pycis/wrappers/base_wrapper.py
+++ pycis/wrappers/base_wrapper.py
@@ -17,7 +17,7 @@ class BaseWrapper(object):
 
     @property
     def name(self):
-        class_name = self.__class__.__name__.lower().strip('wrapper')
+        class_name = self.__class__.__name__.lower().replace('wrapper', '')
         return class_name
 
     def get_streams(self, media):
