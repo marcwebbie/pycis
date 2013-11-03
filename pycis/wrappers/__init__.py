5b548742e855e8fca44100fc9f7bcc1b5adcb5ae
marcwebbie <marcwebbie@gmail.com>
2013-11-03 01:28:52 -0300

    Refactored wrappers __init__ doc

--- pycis/wrappers/__init__.py
+++ pycis/wrappers/__init__.py
@@ -6,16 +6,16 @@ __all__ = [os.path.basename(f)[:-3]
            for f in glob.glob(os.path.dirname(__file__) + "/*.py")]
 
 
-def _import_all_crawler_files():
+def _import_all_wrapper_files():
     __import__(__name__, globals(), locals(), __all__, 0)
 
 
 def get_wrapper_list():
     """ Get all classes with names ending with 'Wrapper' from
-    the module extractors directory
+    the module wrappers directory
     instantiate it and return a list with all the instances
     """
-    _import_all_crawler_files()
+    _import_all_wrapper_files()
 
     wrapper_instance_list = []
     for mod in [v for k, v in globals().items() if k in __all__]:
